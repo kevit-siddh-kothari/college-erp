09dea39079c8c7fb0d38d8d09d533d95d3eb3c77
siddh <siddh.kothari@kevit.io>
2024-08-14 11:50:10 +0530

    testing prettier

--- src/db-connection/mongodb-connection.ts
+++ src/db-connection/mongodb-connection.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
-const connectionToDb: (url: string | undefined) => Promise<void> = async (url) => {
+const connectionToDb: (url: string | undefined) => Promise<void> = async url => {
   if (url) {
     await mongoose
       .connect(url)
       .then(() => console.log(`mongodb connected sucessfully !`))
-      .catch((err) => console.log(err));
+      .catch(err => console.log(err));
     return;
   }
   console.log(`mongodb failed to connect`);
