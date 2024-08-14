e8088414f91aeea825bd8a8562c2ffbc4571f1c8
siddh <siddh.kothari@kevit.io>
2024-08-14 11:56:59 +0530

    testing prettier

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
-import { connectionToDb } from './db-connection/mongodb-connection';
+import { connectionToDb } from "./db-connection/mongodb-connection"
 import express from 'express';
 dotenv.config();
 
-const connectionUrl: string | undefined =    process.env.MONGODB_URL;
+const connectionUrl: string | undefined =    process.env.MONGODB_URL
 const port: string | undefined = process.env.PORT;
 
-connectionToDb(connectionUrl);
+connectionToDb(connectionUrl)
 
-const app = express();
+const app =           express();
 
 export { app, port };
